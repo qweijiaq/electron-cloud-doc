06da4c7dfae3d0e99234f99076054fcd16164187
weijia <2112025700@qq.com>
2024-02-19 19:34:55 +0800

    feat: 引入图标库

--- src/components/FileSearch.jsx
+++ src/components/FileSearch.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faClose } from "@fortawesome/free-solid-svg-icons";
 
 const FileSearch = ({ title, onFileSearch }) => {
   const [inputActive, setInputActive] = useState(false);
@@ -33,41 +35,35 @@ const FileSearch = ({ title, onFileSearch }) => {
   }, [inputActive]);
 
   return (
-    <div className="alert alert-primary">
+    <div className="alert alert-primary d-flex justify-content-between align-items-center mb-0">
       {!inputActive && (
-        <div className="d-flex justify-content-between align-items-center">
+        <>
           <span>{title}</span>
           <button
             type="button"
-            className="btn btn-primary"
+            className="icon-button"
             onClick={() => {
               setInputActive(true);
             }}
           >
-            搜索
+            <FontAwesomeIcon icon={faSearch} size="lg" title="搜索" />
           </button>
-        </div>
+        </>
       )}
       {inputActive && (
-        <div className="row">
-          <div className="col-8">
-            <input
-              className="form-control"
-              value={value}
-              ref={node}
-              onChange={(e) => {
-                setValue(e.target.value);
-              }}
-            />
-          </div>
-          <button
-            type="button"
-            className="btn btn-primary col-4"
-            onClick={closeSearch}
-          >
-            关闭
+        <>
+          <input
+            className="form-control"
+            value={value}
+            ref={node}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
+          />
+          <button type="button" className="icon-button" onClick={closeSearch}>
+            <FontAwesomeIcon icon={faClose} title="关闭" size="lg" />
           </button>
-        </div>
+        </>
       )}
     </div>
   );
