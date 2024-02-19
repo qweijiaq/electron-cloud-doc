965c06030483b88dfe6b8acacb478bf5f721a092
weijia <2112025700@qq.com>
2024-02-19 19:00:08 +0800

    feat: 初步实现 FileSearch 组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import FileSearch from "./components/FileSearch";
 
 function App() {
   return (
     <div className="App">
       <div className="row">
-        <div className="col-3 bg-danger left-panel">
-          <h1>左侧</h1>
+        <div className="col-3 left-panel">
+          <FileSearch title="我的云文档" onFileSearch={() => {}} />
         </div>
         <div className="col-9 bg-primary right-panel">
           <h1>右侧</h1>
